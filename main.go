266706d45f5d6d88501643bced98a3de5166faa1
kautsar_masuara <kautsarmasuara@gmail.com>
2022-09-08 19:44:37 +0800

    update unit test

--- main.go
+++ main.go
@@ -1,5 +1,67 @@
 package main
 
-func Add(a int, b int) int {
-	return a + b
+import (
+	"fmt"
+	"strconv"
+)
+
+/*
+	Add the value to array
+	return array of string
+*/
+func AddValue(arr []string, word string) []string {
+	if word == "Snow" {
+		arr = append(arr, word)
+	} else if word == "Jon" {
+		arr[1] = word
+	}
+	return arr
+}
+
+/*
+	Add the first value on array
+	return array of string
+*/
+func AddFirst(arr []string, word string) []string {
+	res := []string{}
+	res = append(res, word)
+	for _, v := range arr {
+		res = append(res, v)
+	}
+	return res
+}
+
+/*
+	Count the plate numbers are odd and oven
+	return string
+*/
+func CheckISOddOven(plate string) string {
+	arr := []string{}
+	tmp := ""
+
+	//  To split the string to array
+	for i := 0; i < len(plate); i++ {
+		if string(plate[i]) == ";" {
+			arr = append(arr, tmp)
+			tmp = ""
+		} else if i == len(plate)-1 {
+			tmp += string(plate[i])
+			arr = append(arr, tmp)
+		} else {
+			tmp += string((plate[i]))
+		}
+	}
+
+	// Check odd and oven in array
+	odd := 0
+	oven := 0
+	for _, v := range arr {
+		num, _ := strconv.Atoi(v)
+		if num%2 == 1 {
+			odd++
+		} else if num%2 == 0 {
+			oven++
+		}
+	}
+	return fmt.Sprintf("plat genap sebanyak %v dan plat ganjil sebanyak %v", oven, odd)
 }
--- main_test.go
+++ main_test.go
@@ -1,17 +1,41 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestAdd(t *testing.T) {
-	result1 := Add(2, 3)
-	result2 := Add(10, 2)
-	result3 := Add(23, 21)
+func testingArr(arrCompare []string, res []string) bool {
+	flag := true
+	for i, item := range res {
+		if arrCompare[i] != item {
+			flag = false
+		}
+	}
+	return flag
+}
 
-	if result1 != 5 {
-		t.Error("Failed equal 5")
-	} else if result2 != 12 {
-		t.Error("Failed equal 12")
-	} else if result3 != 44 {
-		t.Error("Failed equal 44")
+func TestVocal(t *testing.T) {
+	// Test 1 add normaly value on array
+	arr1 := [...]string{"Rhaegar"}
+	arrCompare1 := []string{"Rhaegar", "Snow"}
+	res1 := AddValue(arr1[:], "Snow")
+
+	// Test 2 add value at the first index on array
+	arr2 := []string{"Snow"}
+	arrCompare2 := []string{"jon", "Snow"}
+	res2 := AddFirst(arr2, "jon")
+
+	// Test 3 checking odd or oven on the number of plate
+	arr3 := "2341;3429;864;1309;1276"
+	arrCompare3 := "plat genap sebanyak 2 dan plat ganjil sebanyak 3"
+
+	if testingArr(arrCompare1, res1) != true {
+		t.Error("failed test 1")
+	}
+	if testingArr(arrCompare2, res2) != true {
+		t.Error("failed test 2")
+	}
+	if CheckISOddOven(arr3) != arrCompare3 {
+		t.Error("failed test 3")
 	}
 }
